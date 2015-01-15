362b1aeb06a97db93ca4909330dad8846a515687
hslee <muckl@paran.com>
2015-01-16 00:30:52 +0900

    make PSCalTest with jUnit

---
+++ PSCalTest/src/com/pnstars/android/test/CalParserTest.java
@@ -0,0 +1,20 @@
+package com.pnstars.android.test;
+
+import android.test.suitebuilder.annotation.SmallTest;
+import junit.framework.TestCase;
+
+public class CalParserTest extends TestCase {
+
+	public CalParserTest(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	@SmallTest
+	public void testSetFormulaToBoundary() {
+		assertTrue("Test", false);
+	}
+}
