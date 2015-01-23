b814b6262906ac9b1225b0cc7e924cf78284f097
hslee <hslee@digitalzone.co.kr>
2015-01-23 20:55:54 +0900

    Change Test Case for MainActivity.class

--- PSCalTest/src/com/pnstars/android/test/CalMainActivityTest.java
+++ PSCalTest/src/com/pnstars/android/test/CalMainActivityTest.java
@@ -1,5 +1,10 @@
 package com.pnstars.android.test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
 import android.widget.Button;
@@ -12,6 +17,16 @@ public class CalMainActivityTest extends
 		ActivityInstrumentationTestCase2<MainActivity> {
 
 	MainActivity mActivity;
+	Button btnN0;
+	Button btnN1;
+	Button btnN4;
+	Button btnN8;
+	Button btnNa;
+	Button btnNb;
+	Button btnNf;
+	Button btnDel;
+	Button btnAC;
+	EditText tvFormula;
 	
 	public CalMainActivityTest() {
 		super(MainActivity.class);
@@ -22,15 +37,37 @@ public class CalMainActivityTest extends
 		
 		setActivityInitialTouchMode(false);
 		mActivity = getActivity();
+		
+		btnN0	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDgt0);
+		btnN1	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDgt1);
+		btnN4	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDgt4);
+		btnN8	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDgt8);
+		btnNa	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnHexaA);
+		btnNb	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnHexaB);
+		btnNf	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnHexaF);
+		btnDel = (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDel);
+		btnAC	= (Button) mActivity.findViewById(com.pnstars.android.R.id.btnAC);
+		tvFormula = (EditText) mActivity.findViewById(com.pnstars.android.R.id.tvFormula);
 	}
 
 	@UiThreadTest
 	public void testKeyInput() {
-		Button btnDgt0 = (Button) mActivity.findViewById(com.pnstars.android.R.id.btnDgt0);
-		EditText tvFormula = (EditText) mActivity.findViewById(com.pnstars.android.R.id.tvFormula);
 		
-		btnDgt0.performClick();
-		PNSDbg.d("" + tvFormula.getText().toString());
-		assertEquals("key input test", "0", tvFormula.getText().toString());
+		HashMap<String, ArrayList<Button>> testBed = new HashMap<String, ArrayList<Button>>() {{
+			put( "0", new ArrayList<Button>() {{ add(btnN0); }} );
+		}};
+		
+		Iterator it = testBed.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pairs = (Map.Entry)it.next();
+			String expValue = (String) pairs.getKey();
+			ArrayList<Button> btns = (ArrayList<Button>) pairs.getValue();
+			for (Button btn : btns) {
+				btn.performClick();
+			}
+			PNSDbg.d("expValue : " + expValue);
+			assertEquals("key input test", expValue, tvFormula.getText().toString());
+			btnAC.performClick();
+		}
 	}
 }
